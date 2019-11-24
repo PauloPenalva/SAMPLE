4f8d4f1d2f8c159179d20a0eaa49211a5d5383d3
Paulo Penalva <paulo.penalva@gmail.com>
2019-11-24 19:29:41 -0300

    Inicio Projeto

---
+++ server.js
@@ -0,0 +1,30 @@
+//@ts-nocheck
+const express = require("express");
+const express_ui5 = require("express-sapui5");
+
+let oConfig = {
+    neoApp: require("./neo-app.json"),
+    destinations: require("./neo-dest.json"),
+    version: "1.69.0"
+};
+
+// initialize environment variables
+require("dotenv").config();
+
+// proxy initializtion
+if (process.env.HTTP_PROXY) {
+    const HttpsProxyAgent = require("https-proxy-agent");
+    oConfig.agent = new HttpsProxyAgent(process.env.HTTP_PROXY);
+}
+
+let app = express();
+
+//sapui5 middleware
+app.use(express_ui5(oConfig));
+
+//static paths
+//["appconfig", "resources"].forEach(function (sPath) {
+//    app.use("/" + sPath, express.static(sPath));
+//});
+
+app.listen(process.env.PORT || 3000);
